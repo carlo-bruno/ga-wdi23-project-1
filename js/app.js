a65d472e1347c7a09c9faf7747bdba09944cba9d
Carlo Bruno <carlofbruno@gmail.com>
2019-02-05 11:30:29 -0800

    trying dom implementation, got board to show in dom

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 console.log("app.js connected");
 
 const game = document.querySelector("#game");
-const ctx = game.getContext("2d");
-
-const colors = ["#bbb", "red"];
 
 const ROW = 20;
 const COL = 10;
 const SIZE = 20;
+
+let colors = [""];
 
 // create board
 let board = [];
@@ -15,26 +14,23 @@ for (let r = 0; r < ROW; r++) {
   board.push(new Array(COL).fill(0));
 }
 
-// draw indivitual cell
-function drawCell(x, y, value) {
-  ctx.fillStyle = colors[value];
-  ctx.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+console.log(board);
 
-  ctx.strokeStyle = "#ddd";
-  ctx.strokeRect(x * SIZE, y * SIZE, SIZE, SIZE);
-}
-
-let piece = [[0, 0, 0], [1, 1, 1], [0, 1, 0]]; // t - shape
-
-// * @param matrix -> any grid, tetrominoes and board
-// go through each row and column, change cells fill color to value
-function drawMatrix(matrix, position) {
-  matrix.forEach((row, y) => {
-    row.forEach((value, x) => {
-      drawCell(x + position.x, y + position.y, value);
-    });
+// show board to the DOM
+board.forEach((row, i) => {
+  let rowEl = document.createElement("div");
+  rowEl.classList.add("row" + i);
+  row.forEach((cell, i) => {
+    let cellEl = document.createElement("div");
+    cellEl.classList.add("cell");
+    cellEl.classList.add(cell);
+    // console.log(cellEl);
+    rowEl.appendChild(cellEl);
   });
-}
+  // debugger;
+  game.appendChild(rowEl);
+});
 
-drawMatrix(board, { x: 0, y: 0 });
-drawMatrix(piece, { x: 5, y: 3 });
+// color each cell
+
+//color matrix
